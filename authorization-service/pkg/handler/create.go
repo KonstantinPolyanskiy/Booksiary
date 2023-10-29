aa31a3e720a3f381e80e023b37d7e8c7ebac56c5
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-10-29 23:00:34 +0300

    implemented the test logic of processing the user creation route

---
+++ authorization-service/pkg/handler/create.go
@@ -0,0 +1,49 @@
+package handler
+
+import (
+	custom_response "Booksiary/authorization-service/internal/custom-response"
+	"Booksiary/authorization-service/internal/types"
+	"errors"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"io"
+	"net/http"
+)
+
+func (h *Handler) createUser(chiCtx *chi.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var input types.User
+		err := render.DecodeJSON(r.Body, &input)
+		if errors.Is(err, io.EOF) {
+			h.L.Error("тело пустое")
+			custom_response.NewErrorResponse(w, r, http.StatusBadRequest, "empty body")
+			return
+		}
+		if err != nil {
+			h.L.Error("некорректное тело")
+			custom_response.NewErrorResponse(w, r, http.StatusBadRequest, "неправильное тело")
+			return
+		}
+
+		id, err := h.Service.Creator.User(input)
+		if err != nil {
+			h.L.Error("idk")
+			custom_response.NewErrorResponse(w, r, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		responseUserCreateOK(w, r, input, id)
+
+	}
+}
+func (h *Handler) createUserCallback(chi *chi.Context) http.HandlerFunc {
+	//TODO: коллбек на создание пользователя
+	panic("сделай")
+}
+
+func responseUserCreateOK(wr http.ResponseWriter, r *http.Request, create types.User, id int) {
+	render.JSON(wr, r, types.UserCreate{
+		Id:   id,
+		User: create,
+	})
+}
