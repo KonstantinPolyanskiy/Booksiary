27544977bc65e717bf5d7767d59db3c02eae2603
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-10-31 00:16:06 +0300

    declared Sever interface and registered it

--- authorization-service/pkg/service/service.go
+++ authorization-service/pkg/service/service.go
@@ -2,14 +2,24 @@ package service
 
 import "Booksiary/authorization-service/internal/types"
 
+type Saver interface {
+	// Save сохранение пользователя на уровне бизнес-логики
+	Save(user types.SaveUser) (int, error)
+}
 type Creator interface {
 	// UserCode создает код подтверждения почты
-	UserCode(user types.User) error
+	UserCode(user types.SaveUser) error
+	// CheckCode проверяет, валиден ли код, и если да - возвоащает пользователя для сохранения
+	CheckCode(code types.Code) (types.SaveUser, error)
 }
 type Service struct {
 	Creator
+	Saver
 }
 
 func NewService() *Service {
-	return &Service{Creator: NewCreatorService()}
+	return &Service{
+		Creator: NewCreatorService(),
+		Saver:   NewSaver(),
+	}
 }
