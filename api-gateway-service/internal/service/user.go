9dd4a0758c58a50486fe9d092c6060968d2ac926
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-02 23:37:52 +0300

    using Director in UserService

--- api-gateway-service/internal/service/user.go
+++ api-gateway-service/internal/service/user.go
@@ -20,13 +20,8 @@ func NewUserService(addrMap ProxyAddrMap) *UserService {
 	return &UserService{ProxyAddrMap: addrMap}
 }
 
-func (s *UserService) Redirect(w http.ResponseWriter, r *http.Request) {
-	director := func(request *http.Request) {
-		h := s.M["/user"] // Получение хоста/порта сервиса
-
-		request.URL.Scheme = "http"
-		request.URL.Host = h.Host + ":" + h.Port
-	}
+func (s *UserService) Redirect(w http.ResponseWriter, r *http.Request, root Root) {
+	director := NewDirector(root, s.M)
 
 	proxy := &httputil.ReverseProxy{Director: director}
 
