63d58f2af241a9d4b07fc7470f7164ac090e2501
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-02 19:47:31 +0300

    api gateway must accept a string like /<service_name>

--- api-gateway-service/internal/handler/proxy.go
+++ api-gateway-service/internal/handler/proxy.go
@@ -1,18 +1,18 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 )
 
 func (h *Handler) proxy(w http.ResponseWriter, r *http.Request) {
-	switch path(r) {
-	case "user":
+	switch rootPath(r.URL.Path) {
+	case "/user":
 		h.service.Redirect(w, r)
 	default:
-		h.service.Redirect(w, r)
-		/*w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "")*/
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "")
 	}
 }
 
@@ -20,4 +20,28 @@ func path(r *http.Request) string {
 	index := strings.LastIndex(r.RequestURI, "/") + 1
 	servicePath := r.RequestURI[index:]
 	return servicePath
+}
+
+func rootPath(path string) string {
+	var slashCount int
+	var root string
+
+	for _, ch := range path {
+		if slashCount == 2 {
+			root += string(ch)
+		}
+
+		if ch == '/' {
+			if slashCount == 1 {
+				slashCount++
+				root += string(ch)
+			} else {
+				slashCount++
+			}
+		}
+	}
+
+	root = root[:len(root)-1]
+
+	return root
 }
