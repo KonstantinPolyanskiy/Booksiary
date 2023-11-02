7dc1e52fa66cdeea9c1162f16416472e557efba1
KonstantinPolyanskiy <polyanskiy.main@mail.ru>
2023-11-02 14:59:02 +0300

    json body acceptance check

--- user-service/internal/handler/create.go
+++ user-service/internal/handler/create.go
@@ -1,11 +1,29 @@
 package handler
 
 import (
+	"Booksiary/user-service/internal"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
+type User struct {
+	Name    string `json:"name"`
+	Surname string `json:"surname"`
+}
+
 func (h *Handler) createUser(ctx *gin.Context) {
-	h.service.Creator.Create()
-	ctx.Status(http.StatusOK)
+	var user User
+	err := ctx.BindJSON(&user)
+	if err != nil {
+		internal.ErrorResponse(ctx, http.StatusBadRequest, "неправильное тело запроса")
+		return
+	}
+
+	id, err := h.service.Creator.Create()
+	if err != nil {
+		internal.ErrorResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx.JSON(http.StatusOK, id)
 }
