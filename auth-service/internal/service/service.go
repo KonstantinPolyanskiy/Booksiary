80b603e0991f7be22b84da31a153bc4eb3202f35
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-04 20:06:51 +0300

    Repository now implement AuthRepo interface

--- auth-service/internal/service/service.go
+++ auth-service/internal/service/service.go
@@ -1,9 +1,13 @@
 package service
 
+import (
+	"Booksiary/auth-service/internal/repository"
+)
+
 type Service struct {
 	*AuthService
 }
 
-func NewService() *Service {
-	return &Service{AuthService: NewAuthService()}
+func NewService(repos *repository.Repository) *Service {
+	return &Service{AuthService: NewAuthService(repos)}
 }
