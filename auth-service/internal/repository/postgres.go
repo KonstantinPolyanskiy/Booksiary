593f8fb92361974e802c36fd2481c8ae3549526e
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-04 20:04:03 +0300

    func for create postgres db

---
+++ auth-service/internal/repository/postgres.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"fmt"
+	_ "github.com/jackc/pgx/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewPostgresDB(cfg PostgresConfig) (*sqlx.DB, error) {
+	db, err := sqlx.Open("pgx",
+		fmt.Sprintf(
+			"host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+			cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, err
+}
