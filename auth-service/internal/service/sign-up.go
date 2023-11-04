6324b422e6640278ac70ea844a4b7f069644d9d8
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-04 20:07:37 +0300

    the simplest implementation of user saving on the Service layer

--- auth-service/internal/service/sign-up.go
+++ auth-service/internal/service/sign-up.go
@@ -2,21 +2,39 @@ package service
 
 import (
 	"Booksiary/auth-service/internal/domain"
+	"Booksiary/auth-service/internal/repository"
+	"Booksiary/auth-service/pkg/password"
+	"github.com/google/uuid"
 )
+
+const passwordSalt = "testsalt-dsgfkhergtfdrfwm"
 
 // Creator создает пользователя в системе, сохраняя авторизационные данные
 // и передавая их сервису Users
 type Creator interface {
-	Create(user domain.User) (int, error)
+	Create(user domain.UserRegistrationData) (int, error)
 }
 
 type AuthService struct {
+	repos *repository.Repository
 }
 
-func NewAuthService() *AuthService {
-	return &AuthService{}
+func NewAuthService(repos *repository.Repository) *AuthService {
+	return &AuthService{repos: repos}
 }
 
-func (s *AuthService) Create(user domain.User) (int, error) {
-	return 1, nil
+func (s *AuthService) Create(userData domain.UserRegistrationData) (uuid.UUID, error) {
+	registeredUser := domain.RegisteredUser{
+		Name:         userData.Name,
+		Surname:      userData.Surname,
+		Login:        userData.Login,
+		PasswordHash: password.Hash(userData.Password, passwordSalt),
+		Email:        userData.Email,
+	}
+
+	userUUID, err := s.repos.AuthRepo.Record(registeredUser)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	return userUUID, err
 }
