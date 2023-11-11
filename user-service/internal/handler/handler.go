0b4477b83f7d84de93d0610791652bcf30f0e892
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-11 23:05:42 +0300

    delete unused routes

--- user-service/internal/handler/handler.go
+++ user-service/internal/handler/handler.go
@@ -21,11 +21,7 @@ func (h *Handler) Init() *gin.Engine {
 		user := api.Group("/user")
 		{
 			user.POST("/create", h.createUser)
-			user.GET(":id", h.getUser)
-			user.DELETE(":id", h.deleteUser)
-			user.PUT(":id", h.updateUser)
-
-			user.POST("/user-data", h.createUserWithExternal)
+			user.POST("/code", h.createUserCallback)
 		}
 	}
 
