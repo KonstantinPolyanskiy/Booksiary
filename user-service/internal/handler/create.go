0a4f8becc9199db29b92acffdff24cab949c2990
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-11 22:54:37 +0300

    implemented function of user registration processing

--- user-service/internal/handler/create.go
+++ user-service/internal/handler/create.go
@@ -1,14 +1,52 @@
 package handler
 
 import (
+	"Booksiary/user-service/internal/domain"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func (h *Handler) createUser(ctx *gin.Context) {
-	h.service.Creator.Create()
-	ctx.Status(http.StatusOK)
+	var input domain.UserRegistrationData
+	var err error
+
+	err = ctx.BindJSON(&input)
+	if err != nil {
+		NewErrResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+	err = h.service.Registration.SignUp(input)
+	if err != nil {
+		NewErrResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx.Status(http.StatusAccepted)
+}
+
+func (h *Handler) createUserCallback(ctx *gin.Context) {
+	code, err := strconv.Atoi(ctx.Query("code"))
+	if err != nil {
+		NewErrResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+	if err != nil {
+		NewErrResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	userUUID, err := h.service.SignUpCallback(code)
+	if err != nil {
+		NewErrResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx.JSON(http.StatusOK, map[string]interface{}{
+		"status": "зарегестрирован",
+		"uuid":   userUUID,
+	})
 }
 
 func (h *Handler) createUserWithExternal(ctx *gin.Context) {
@@ -19,4 +57,13 @@ func (h *Handler) createUserWithExternal(ctx *gin.Context) {
 		return
 	}
 	log.Print(input)
+}
+
+func getCode(ctx *gin.Context, keyCode string) (int, error) {
+	code, err := strconv.Atoi(ctx.Param(keyCode))
+	if err != nil {
+		return 0, err
+	}
+
+	return code, nil
 }
