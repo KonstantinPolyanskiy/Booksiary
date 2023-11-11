71f623ea0df8d0518637f2621b80509c3c80bc3f
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-11 23:23:45 +0300

    applied changes User microservice

--- user-service/cmd/app/main.go
+++ user-service/cmd/app/main.go
@@ -3,15 +3,51 @@ package main
 import (
 	"Booksiary/user-service/internal/handler"
 	http_server "Booksiary/user-service/internal/http-server"
+	"Booksiary/user-service/internal/mail"
+	"Booksiary/user-service/internal/repository"
+	"Booksiary/user-service/internal/repository/badger"
+	"Booksiary/user-service/internal/repository/postgres"
 	"Booksiary/user-service/internal/service"
+	mailExternal "github.com/xhit/go-simple-mail/v2"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func main() {
-	services := service.NewService()
+	mailClient, err := mail.NewMailClient(mail.Config{
+		Host:           "smtp.mail.ru",
+		Username:       "work.polyanskiy@mail.ru",
+		Password:       "fApNn2LEpCzhzA6L9pKe",
+		EmailAddress:   "work.polyanskiy@mail.ru",
+		Port:           587,
+		Encryption:     mailExternal.EncryptionTLS,
+		ConnectTimeout: 15 * time.Second,
+		SendTimeout:    15 * time.Second,
+	})
+	if err != nil {
+		log.Fatalf("Ошибка в создании email клиента - %s", err)
+	}
+	memoryDB, err := badger.NewBadgerDB()
+	if err != nil {
+		log.Fatalf("Ошибка в инициализации badger - %v\n", err)
+	}
+	db, err := postgres.NewPostgresDB(postgres.Config{
+		Host:     "localhost",
+		Port:     "5480",
+		Username: "konstantin",
+		Password: "user12345",
+		DBName:   "postgres",
+		SSLMode:  "disable",
+	})
+	if err != nil {
+		log.Fatalf("Ошибка в инициализации postgres - %v\n", err)
+	}
+
+	repository := repository.NewRepository(db, memoryDB)
+	services := service.NewService(repository, *mailClient)
 	handlers := handler.NewHandler(services)
 
 	server := new(http_server.Server)
