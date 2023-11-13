6079650e6cbb799de44cca5cedc663db1238fe51
Santixtx <danil.shirin.04@mail.ru>
2023-11-13 11:05:33 +0300

    delete unused code

--- authorization-service/config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import "time"
-
-type ServerConfig struct {
-	Port         string
-	WriteTimeout time.Duration
-	ReadTimeout  time.Duration
-}
--- authorization-service/internal/lib/random/mail-code.go
+++
@@ -1,7 +0,0 @@
-package random
-
-import "math/rand"
-
-func Code() int {
-	return 1000 + rand.Intn(10000-1000+1)
-}
